ec57403339045ec16358677c36f4cab5878346da
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-01-30 17:56:43 +0300

    Ecosystem sayfası için yeni SlugPage bileşeni eklendi; kart verileri için dinamik içerik sağlandı ve çeviriler güncellendi.

--- src/app/[locale]/ecosystem/[slug]/page.jsx
+++ src/app/[locale]/ecosystem/[slug]/page.jsx
@@ -1,7 +1,98 @@
+import SlugPage from "@/components/ecosystem/SlugPage";
+import { getTranslations } from "next-intl/server";
 import React from "react";
 
 export default async function page({ params }) {
   const slug = (await params).slug;
+  const t = await getTranslations("EcosystemPage");
+  const cards = [
+    {
+      title: t("cards.weeZard.shortTitle"),
+      slug: t("cards.weeZard.slug"),
+      description: t("cards.weeZard.shortDescription"),
+      sections: {
+        howItWorks: {
+          title: t("cards.weeZard.sections.howItWorks.title"),
+          content: t("cards.weeZard.sections.howItWorks.content"),
+        },
+        whyUse: {
+          title: t("cards.weeZard.sections.whyUse.title"),
+          content: t("cards.weeZard.sections.whyUse.content"),
+        },
+        contribution: {
+          title: t("cards.weeZard.sections.contribution.title"),
+          content: t("cards.weeZard.sections.contribution.content"),
+        },
+      },
+      detailedText: t("cards.weeZard.detailedText"),
+      date: t("cards.weeZard.date"),
+    },
+    {
+      title: t("cards.weeCard.shortTitle"),
+      slug: t("cards.weeCard.slug"),
+      description: t("cards.weeCard.shortDescription"),
+      sections: {
+        howItWorks: {
+          title: t("cards.weeCard.sections.howItWorks.title"),
+          content: t("cards.weeCard.sections.howItWorks.content"),
+        },
+        whyUse: {
+          title: t("cards.weeCard.sections.whyUse.title"),
+          content: t("cards.weeCard.sections.whyUse.content"),
+        },
+        contribution: {
+          title: t("cards.weeCard.sections.contribution.title"),
+          content: t("cards.weeCard.sections.contribution.content"),
+        },
+      },
+      detailedText: t("cards.weeCard.detailedText"),
+      date: t("cards.weeCard.date"),
+    },
+    {
+      title: t("cards.weeSale.shortTitle"),
+      slug: t("cards.weeSale.slug"),
+      description: t("cards.weeSale.shortDescription"),
+      sections: {
+        howItWorks: {
+          title: t("cards.weeSale.sections.howItWorks.title"),
+          content: t("cards.weeSale.sections.howItWorks.content"),
+        },
+        whyUse: {
+          title: t("cards.weeSale.sections.whyUse.title"),
+          content: t("cards.weeSale.sections.whyUse.content"),
+        },
+        contribution: {
+          title: t("cards.weeSale.sections.contribution.title"),
+          content: t("cards.weeSale.sections.contribution.content"),
+        },
+      },
+      detailedText: t("cards.weeSale.detailedText"),
+      date: t("cards.weeSale.date"),
+    },
+    {
+      title: t("cards.weekobi.shortTitle"),
+      slug: t("cards.weekobi.slug"),
+      description: t("cards.weekobi.shortDescription"),
+      sections: {
+        howItWorks: {
+          title: t("cards.weekobi.sections.howItWorks.title"),
+          content: t("cards.weekobi.sections.howItWorks.content"),
+        },
+        whyUse: {
+          title: t("cards.weekobi.sections.whyUse.title"),
+          content: t("cards.weekobi.sections.whyUse.content"),
+        },
+        contribution: {
+          title: t("cards.weekobi.sections.contribution.title"),
+          content: t("cards.weekobi.sections.contribution.content"),
+        },
+      },
+      detailedText: t("cards.weekobi.detailedText"),
+      date: t("cards.weekobi.date"),
+    },
+  ];
   console.log(slug);
-  return <div>page</div>;
+  const card = cards.filter((item) => item.slug === slug);
+  console.log(card);
+  return <SlugPage card={card[0]} />;
 }
--- src/components/ecosystem/VerticalTimeline.jsx
+++ src/components/ecosystem/VerticalTimeline.jsx
@@ -12,46 +12,50 @@ export default function VerticalTimelineContainer() {
   const t = useTranslations("EcosystemPage");
   const cards = [
     {
-      title: t("cards.WE3DUCATION.title"),
-      description: t("cards.WE3DUCATION.description"),
-      date: t("cards.WE3DUCATION.date"),
+      title: t("cards.wE3DUCATION.title"),
+      description: t("cards.wE3DUCATION.description"),
+      date: t("cards.wE3DUCATION.date"),
       icon: FaBookOpen,
     },
     {
-      title: t("cards.WE3STATE.title"),
-      description: t("cards.WE3STATE.description"),
-      date: t("cards.WE3STATE.date"),
+      title: t("cards.wE3STATE.title"),
+      description: t("cards.wE3STATE.description"),
+      date: t("cards.wE3STATE.date"),
       icon: FaChartLine,
     },
     {
-      title: t("cards.WE3NERGY.title"),
-      description: t("cards.WE3NERGY.description"),
-      date: t("cards.WE3NERGY.date"),
+      title: t("cards.wE3NERGY.title"),
+      description: t("cards.wE3NERGY.description"),
+      date: t("cards.wE3NERGY.date"),
       icon: BiSolidZap,
     },
     //
     {
-      title: t("cards.WE3NERGY.title"),
-      description: t("cards.WE3NERGY.description"),
-      date: t("cards.WE3NERGY.date"),
+      title: t("cards.weeZard.shortTitle"),
+      slug: t("cards.weeZard.slug"),
+      description: t("cards.weeZard.shortDescription"),
+      date: t("cards.weeZard.date"),
       icon: BiSolidZap,
     },
     {
-      title: t("cards.WE3NERGY.title"),
-      description: t("cards.WE3NERGY.description"),
-      date: t("cards.WE3NERGY.date"),
+      title: t("cards.weeCard.shortTitle"),
+      slug: t("cards.weeCard.slug"),
+      description: t("cards.weeCard.shortDescription"),
+      date: t("cards.weeCard.date"),
       icon: BiSolidZap,
     },
     {
-      title: t("cards.WE3NERGY.title"),
-      description: t("cards.WE3NERGY.description"),
-      date: t("cards.WE3NERGY.date"),
+      title: t("cards.weeSale.shortTitle"),
+      slug: t("cards.weeSale.slug"),
+      description: t("cards.weeSale.shortDescription"),
+      date: t("cards.weeSale.date"),
       icon: BiSolidZap,
     },
     {
-      title: t("cards.WE3NERGY.title"),
-      description: t("cards.WE3NERGY.description"),
-      date: t("cards.WE3NERGY.date"),
+      title: t("cards.weekobi.shortTitle"),
+      slug: t("cards.weekobi.slug"),
+      description: t("cards.weekobi.shortDescription"),
+      date: t("cards.weekobi.date"),
       icon: BiSolidZap,
     },
   ];
@@ -65,20 +69,24 @@ export default function VerticalTimelineContainer() {
           icon={<item.icon className="!h-6 !w-6 text-light-833E15 dark:text-light-f88540 md:!h-8 md:!w-8" />}
           date={item.date}
         >
-          <Link
-            href={`/ecosystem/${item.title}`}
-            className="text-xl font-semibold text-zinc-800 transition-all hover:text-black dark:text-white"
-          >
-            {item.title}
-          </Link>
+          {item.slug ? (
+            <Link
+              href={`/ecosystem/${item.slug}`}
+              className="text-xl font-semibold text-zinc-800 transition-all hover:text-black dark:text-white"
+            >
+              {item.title}
+            </Link>
+          ) : (
+            <h3 className="text-xl font-semibold text-zinc-800 transition-all hover:text-black dark:text-white">{item.title}</h3>
+          )}
           <p className="text-zinc-700 dark:text-white/85">
             {item.description.length <= 192 ? item.description : `${item.description.slice(0, 192)}...`}
-            {item.description.length <= 192 ? (
-              ""
-            ) : (
-              <Link href={`/ecosystem/${item.title}`} className="text-sm text-zinc-800 underline transition-all hover:text-black">
+            {item.description.length > 192 && item.slug ? (
+              <Link href={`/ecosystem/${item.slug}`} className="text-sm text-zinc-800 underline transition-all hover:text-black">
                 Devamını Oku
               </Link>
+            ) : (
+              ""
             )}
           </p>
         </VerticalTimelineElement>

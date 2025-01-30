6ec2e51e26981265eaac065a57923d8a3e5a8135
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-01-30 14:10:44 +0300

    i18n-ally ayarları güncellendi; Türkçe çeviride "Anasayfa" kelimesi "Ev" olarak değiştirildi; PageTitle bileşeninde GSAP animasyonu eklendi.

--- src/components/common/PageTitle.jsx
+++ src/components/common/PageTitle.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+"use client";
+import React, { useRef } from "react";
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -9,15 +10,26 @@ import {
 } from "@/components/ui/breadcrumb";
 import { Slash } from "lucide-react";
 import { useTranslations } from "next-intl";
+import { gsap } from "gsap";
+import { useGSAP } from "@gsap/react";
+
+gsap.registerPlugin(useGSAP);
 
 export default function PageTitle({ title, subtitle }) {
   const t = useTranslations("AboutPage");
+  const pageTitleRef = useRef(null);
+
+  useGSAP(() => {
+    gsap.from(pageTitleRef.current, {
+      opacity: 0,
+      duration: 0.3,
+    });
+  }, []);
+
   return (
     <section className="fluid gridContainer relative mt-32 h-auto py-4 shadow-sm md:mt-20 md:h-[300px]">
       <div className="fluid absolute inset-0 -z-20 bg-page-title-bg-light bg-cover bg-center bg-no-repeat dark:bg-page-title-bg-dark"></div>
-      {/* <div className="to-light-f88540/20 fluid absolute inset-0 -z-20 bg-gradient-to-br from-light-gradient-f7cf68/20 dark:from-dark-0d0d0d dark:to-light-833E15/30"></div> */}
-      {/* <div className="fluid absolute inset-0 -z-20 bg-black/5" /> */}
-      <div className="relative mx-auto flex h-full flex-col items-center justify-center gap-4 px-6 text-center">
+      <div ref={pageTitleRef} className="relative mx-auto flex h-full flex-col items-center justify-center gap-4 px-6 text-center">
         <h1 className="bg-gradient-to-t from-light-833E15 to-dark-808080 bg-clip-text font-dmSans text-5xl font-bold text-transparent xl:text-7xl 2xl:text-8xl">
           {title}
         </h1>

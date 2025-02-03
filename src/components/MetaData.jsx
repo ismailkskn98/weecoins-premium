9da177d4c34c28fec949363f2998a6dcc1413d91
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-02-03 17:30:55 +0300

    55

--- src/components/MetaData.jsx
+++ src/components/MetaData.jsx
@@ -4,7 +4,28 @@ export const customMetaData = {
     "Weecoins Premium: Sürdürülebilir ve yenilikçi yatırım fırsatlarıyla kripto dünyasında öncü. Web sitemizde WCP satın alabilir, projelerimize yatırım yapabilirsiniz. Gelişmiş enerji projeleriyle geleceğe yatırım yapın.",
   robots: "index, follow",
   icons: {
-    icon: "/images/favicon.ico",
+    icon: [
+      {
+        url: "/images/favicon.ico",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        url: "/images/favicon.ico",
+        type: "image/png",
+        sizes: "512x512",
+      },
+      {
+        url: "/images/logo-xl-footer.webp",
+        type: "image/png",
+        sizes: "192x192",
+      },
+      {
+        url: "/images/logo-xl-footer.webp",
+        type: "image/png",
+        sizes: "512x512",
+      },
+    ],
   },
   keywords: ["Weecoins Premium", "kripto para", "sanal para", "blockchain", "kripto yatırımı"],
 };
